4b2371aa102b54a2f26a88e8931bd67c547bbd70
UniverseVG <gmvarun2001@gmail.com>
2025-12-03 20:29:29 +0530

    feat: Introduce draggable Terminal window with controls and a generic window wrapper HOC, and remove default App styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Welcome, Navbar, Dock } from "#components";
+import { Draggable } from "gsap/Draggable";
+import gsap from "gsap";
+import { Terminal } from "#windows";
 
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   );
 };
